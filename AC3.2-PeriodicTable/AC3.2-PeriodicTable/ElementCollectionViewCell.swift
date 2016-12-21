0adae4ace9b6d6ff64ac9c284af4d33d92287853
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-12-21 10:59:15 -0500

    creating nibs and setting up collection view

---
+++ AC3.2-PeriodicTable/AC3.2-PeriodicTable/ElementCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ElementCollectionViewCell.swift
+//  AC3.2-PeriodicTable
+//
+//  Created by Marty Avedon on 12/21/16.
+//  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class ElementCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ AC3.2-PeriodicTable/AC3.2-PeriodicTable/ElementView.swift
@@ -0,0 +1,21 @@
+//
+//  ElementView.swift
+//  AC3.2-PeriodicTable
+//
+//  Created by Marty Avedon on 12/21/16.
+//  Copyright © 2016 Marty Hernandez Avedon. All rights reserved.
+//
+
+import UIKit
+
+class ElementView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
